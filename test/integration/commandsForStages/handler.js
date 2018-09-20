2173faa8f20a5f49c4fd509e17ad15700a918ecb
Teddy Gustiaux <code@gustiaux.com>
2018-09-19 22:21:35 -0400

    Add initial version of integration tests

---
+++ test/integration/commandsForStages/handler.js
@@ -0,0 +1,8 @@
+module.exports.hello = async (event, context) => ({
+    statusCode: 200,
+    body: JSON.stringify({
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event,
+        context,
+    }),
+});
---
+++ test/integration/stages.test.js
@@ -0,0 +1,57 @@
+// External dependencies
+const { expect, use } = require('chai');
+const { spawnSync } = require('child_process');
+const dirtyChai = require('dirty-chai');
+
+use(dirtyChai);
+
+describe('Integration tests of the plugin - using stages only', () => {
+    const folder = 'stages';
+
+    it('should indicate the command as not confirmed if the option is not provided', () => {
+        const subprocess = spawnSync(
+            'node',
+            ['../../../node_modules/serverless/bin/serverless', 'deploy', '--stage', 'prod'],
+            { cwd: `${__dirname}/${folder}` },
+        );
+        const stdout = subprocess.stdout.toString('utf-8');
+        const notConfirmed = stdout.includes('Command not confirmed.');
+        const serverlessError = stdout.includes('Serverless Error');
+        expect(notConfirmed).to.be.true();
+        expect(serverlessError).to.be.true();
+    }).timeout(5000);
+
+    it('should indicate the command as confirmed if the option is provided', () => {
+        const subprocess = spawnSync(
+            'node',
+            [
+                '../../../node_modules/serverless/bin/serverless',
+                'deploy',
+                '--stage',
+                'prod',
+                '--confirm',
+            ],
+            { cwd: `${__dirname}/${folder}` },
+        );
+        const stdout = subprocess.stdout.toString('utf-8');
+        const confirmed = stdout.includes('Command confirmed.');
+        const serverlessError = stdout.includes('Serverless Error');
+        expect(confirmed).to.be.true();
+        expect(serverlessError).to.be.true();
+    }).timeout(5000);
+
+    it('should not do anything if the command does not require confirmation', () => {
+        const subprocess = spawnSync(
+            'node',
+            ['../../../node_modules/serverless/bin/serverless', 'remove'],
+            { cwd: `${__dirname}/${folder}` },
+        );
+        const stdout = subprocess.stdout.toString('utf-8');
+        const confirmed = stdout.includes('Command confirmed.');
+        const notConfirmed = stdout.includes('Command not confirmed.');
+        const serverlessError = stdout.includes('Serverless Error');
+        expect(notConfirmed).to.be.false();
+        expect(confirmed).to.be.false();
+        expect(serverlessError).to.be.true();
+    }).timeout(5000);
+});
